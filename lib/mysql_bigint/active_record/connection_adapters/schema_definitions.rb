9b6245f6461bc5c6999fd35b8a43e8b99d38d694
Ben Woosley <ben.woosley@gmail.com>
2010-08-19 17:25:34 -0400

    Rails wasn't picking up the schema definition extensions, so switch back to alias_method_chain

--- lib/mysql_bigint/active_record/connection_adapters/schema_definitions.rb
+++ lib/mysql_bigint/active_record/connection_adapters/schema_definitions.rb
@@ -4,20 +4,19 @@ module ActiveRecord
       attr_accessor :unsigned
     end
 
-    module UnsignedTableSupport
-      def column(name, type, options = {})
-        super.tap do |column|
+    class TableDefinition
+      def column_with_unsigned(name, type, options = {})
+        column_without_unsigned(name, type, options).tap do |column|
           column[name].unsigned = options[:unsigned]
         end
       end
-    end
+      alias_method_chain :column, :unsigned
 
-    module BigIntTableSupport
       # Appends a primary key definition to the table definition.
       # Can be called multiple times, but this is probably not a good idea.
       # Changed to support the use of bigints as the primary key
-      def primary_key(name, type = :primary_key)
-        return super(name) unless @base.class.to_s =~ /mysql/i
+      def primary_key_with_mysql_bigint(name, type = :primary_key)
+        return primary_key_without_mysql_bigint(name) unless @base.class.to_s =~ /mysql/i
 
         type = native.fetch(type) do
           options = 'DEFAULT NULL auto_increment ' if type =~ /int/
@@ -25,10 +24,7 @@ module ActiveRecord
         end
         column(name, type)
       end
-    end
-
-    class TableDefinition
-      include BigIntTableSupport, UnsignedTableSupport
+      alias_method_chain :primary_key, :mysql_bigint
     end
   end
 end
